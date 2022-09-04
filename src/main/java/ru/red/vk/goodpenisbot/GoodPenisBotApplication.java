3708d070f4f79ae3d36e3e0d6f8ad39049b8e015
ReDestroyDeR <daniil.schroder@gmail.com>
2022-09-05 00:53:35 +0300

    Initial commit

---
+++ src/main/java/ru/red/vk/goodpenisbot/GoodPenisBotApplication.java
@@ -0,0 +1,13 @@
+package ru.red.vk.goodpenisbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodPenisBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodPenisBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/red/vk/goodpenisbot/GoodPenisBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.red.vk.goodpenisbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodPenisBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
